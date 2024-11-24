b3181ac15fdaeace4fcabc77569efd3934b9be48
toshi <toshifumi@alumni.upenn.edu>
2024-11-24 09:13:18 +0900

    Initial checkin. failing to find mappings.

---
+++ src/main/java/net/cransmontana/mccourse/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.cransmontana.mccourse.item;
+
+import net.cransmontana.mccourse.MCCourseMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MCCourseMod.MOD_ID);
+
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
+            ()-> new Item(new Item.Properties().useItemDescriptionPrefix()
+                    .setId(ResourceKey.create(Registries.ITEM, ResourceLocation.parse("modid:"
+                            +MCCourseMod.MOD_ID)))));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
